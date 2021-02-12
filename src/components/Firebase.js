6adef210c471a45c4a4157f5e0ff1c18ad3396d4
Agrim <agrim.tuladhar@gmail.com>
2021-02-12 17:12:14 +0545

    first commit

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-	apiKey: "AIzaSyD2ZI4OGZWm1LjlgUhW6B_pitkcrscTzRo",
-	authDomain: "webimagegallary.firebaseapp.com",
-	databaseURL: "https://webimagegallary.firebaseio.com",
-	projectId: "webimagegallary",
-	storageBucket: "webimagegallary.appspot.com",
-	messagingSenderId: "824109196843",
-	appId: "1:824109196843:web:ced07234a46e7d6d1c2c6e",
-	measurementId: "G-XKHPTHYVLS"
+	
 };
 
 // Initialize Firebase
